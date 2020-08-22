19547abf0fdea51ca5d2754efe92758c19f63287
Artur Yumagulov <zico.13288@gmail.com>
2020-08-22 09:18:04 +0300

    added rubric list

--- bboard/views.py
+++ bboard/views.py
@@ -1,12 +1,18 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 from django.template import loader
 
-from .models import Bb
+from .models import Bb, Rubric
 
 
 def index(requests):
-    template = loader.get_template('bboard/index.html')
-    bbs = Bb.objects.order_by('-published')
-    context = {'bbs': bbs}
-    return HttpResponse(template.render(context, requests))
+    bbs = Bb.objects.all()
+    rubrics = Rubric.objects.all()
+    context = {'bbs': bbs, 'rubrics': rubrics}
+    return render(requests, 'bboard/index.html', context)
 
+def by_rubric(requests, rubric_id):
+    bbs = Bb.objects.filter(rubric=rubric_id)
+    rubrics = Rubric.objects.all()
+    current_rubric = Rubric.objects.get(pk=rubric_id)
+    context = {'bbs': bbs, 'rubrics': rubrics, 'current_rubric': current_rubric}
+    return render(requests, 'bboard/by_rubric.html', context)
